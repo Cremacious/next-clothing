d96a5d44c7cde1f2cf332d62c2ca255e3c0c62e1
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-02-04 15:50:45 -0500

    Product list card now displays content on homepage.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,19 @@
-import { Button } from '@/components/ui/button';
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 
 export const metadata = {
   title: 'Home',
-}
+};
 
 const Homepage = () => {
   return (
     <>
-      Next Clothing
-      <Button>Press me</Button>
+      Home
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
     </>
   );
 };
