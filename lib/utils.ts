e0bb66d0715dab85dbc4a354574dbaba9041753d
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-02-05 14:13:54 -0500

    Created helpers and validator for adding to schema.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 // Convert prisma objects to JS objects
-
 export function convertToPlainObject<T>(object: T): T {
-  return JSON.parse(JSON.stringify(object))
+  return JSON.parse(JSON.stringify(object));
 }
 
+//Format number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split('.');
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
+}
