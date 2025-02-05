924e1c0592291234f6bcd2df553ca9fda36987f1
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-02-05 18:46:11 -0500

    Product page setup to take slug and show relevant details.

--- lib/actions/products.actions.ts
+++ lib/actions/products.actions.ts
@@ -4,7 +4,6 @@ import { prisma } from '@/db/prisma';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 export async function getLatestProducts() {
- 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
@@ -12,4 +11,12 @@ export async function getLatestProducts() {
     },
   });
   return convertToPlainObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: {
+      slug: slug,
+    },
+  });
 }
