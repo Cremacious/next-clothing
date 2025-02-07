3e308abbe0b50adcfb0380ad9465af56e2b2e9cc
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-02-07 16:36:35 -0500

    Seeded user data.

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import { hashSync } from 'bcrypt-ts-edge';
+
 const sampleData = {
+  users: [
+    {
+      name: 'John',
+      email: 'adminemail@test.com',
+      password: hashSync('123456', 10),
+      role: 'admin',
+    },
+    {
+      name: 'Jane',
+      email: 'useremail@test.com',
+      password: hashSync('123456', 10),
+      role: 'user',
+    },
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
