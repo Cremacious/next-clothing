dc7b1c253a935eb56a61b74582cab414d1c71626
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-02-07 10:52:29 -0500

    Product page displays images.

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { getProductBySlug } from '@/lib/actions/products.actions';
 import PageNotFound from '@/app/not-found';
 import ProductPrice from '@/components/shared/product/product-price';
+import ProductImages from '@/components/shared/product/product-images';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -18,7 +19,9 @@ const ProductDetailsPage = async (props: {
     <>
       <section className="">
         <div className="grid grid-cols-1 md:grid-cols-5">
-          <div className="imagesx col-span-2"></div>
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
           <div className="col-span-2 p-5">
             <div className="flex flex-col gap-6">
               <p>
---
+++ components/shared/product/product-images.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+
+const ProductImages = ({ images }: { images: string[] }) => {
+  const [current, setCurrent] = useState(0);
+
+  return (
+    <div className="space-y-4">
+      <Image
+        className="min-h-[300px]"
+        src={images[current]}
+        alt={images[0]}
+        width={1000}
+        height={1000}
+        object-cover={true}
+        object-center={true}
+      />
+      <div className="flex">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrent(index)}
+            className={cn(
+              'border mr-2 cursor-pointer hover:border-orange-600',
+              current === index && ''
+            )}
+          >
+            <Image src={image} alt={image} height={100} width={100} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
