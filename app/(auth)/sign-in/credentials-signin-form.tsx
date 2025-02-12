d5b09427c4044c3d232a6c37e7e647b263132597
Cremacious <chrismackall3@gmail.com>
2025-02-12 13:37:29 -0500

    Credentials import started

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -4,10 +4,28 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
+import { signInWithCredentials } from '@/lib/actions/user.actions';
+import { useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+
 
 const CredentialsSigninForm = () => {
+
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: ''
+  })
+
+  const signInButton = () => {
+    const {pending} = useFormStatus()
+
+    return (
+      <Button disabled={pending} className='text-center'>bb</Button>
+    )
+  }
+
   return (
-    <form className="">
+    <form action={action} className="">
       <div className="space-y-4">
         <div>
           <Label htmlFor="email">Email</Label>
@@ -32,6 +50,12 @@ const CredentialsSigninForm = () => {
         <Button variant="default" className="w-full">
           Sign In
         </Button>
+
+        {data && !data.success && (
+          <div className="text-center text-md">
+            {data.message}
+          </div>
+        )}
         <div className="text-center text-sm text-muted-foreground">
           Don&apos;t have an account? {' '}
           <Link href="/sign-up" target="_self" className="link">
