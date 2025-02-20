b02173cb1235834823b1bd8b552fb56c907d4cf7
Cremacious <chrismackall3@gmail.com>
2025-02-20 18:48:33 -0500

    Payment constants and schema added. User actions can update payment method

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { formatNumberWithDecimal } from './utils';
+import { PAYMENT_METHODS } from './constants';
 
 const currency = z
   .string()
@@ -69,4 +70,13 @@ export const shippingAddressSchema = z.object({
   country: z.string().min(3, 'Country must be at least 3 characters'),
   lat: z.number().optional(),
   lng: z.number().optional(),
-})
+});
+
+export const paymentMethodSchema = z
+  .object({
+    type: z.string().min(1, 'Payment method is required'),
+  })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ['type'],
+    message: 'Invalid Payment Method',
+  });
