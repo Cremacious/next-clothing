0e11bf52d03c05132d74c61a11f629bc7521dd1e
Cremacious <chrismackall3@gmail.com>
2025-02-21 17:24:38 -0500

    Validators for order and order items

--- lib/validators.ts
+++ lib/validators.ts
@@ -80,3 +80,25 @@ export const paymentMethodSchema = z
     path: ['type'],
     message: 'Invalid Payment Method',
   });
+
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, 'User ID is required'),
+  itemsPrice: currency,
+  totalPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  //using data on refine uses data for this field instead of data in all object
+  paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+    message: 'Invalid Payment Method',
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  name: z.string(),
+  price: currency,
+  qty: z.number(),
+});
