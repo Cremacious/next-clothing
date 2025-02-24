2d9e01a7eea2f5f02290e2d8ba04da599e808ebc
Cremacious <chrismackall3@gmail.com>
2025-02-24 16:21:18 -0500

    Order page now uses order details table

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,26 +1,27 @@
-import { getOrderById } from "@/lib/actions/order.actions";
-import {notFound} from 'next/navigation'
-import { ShippingAddress } from "@/types";
-
+import { getOrderById } from '@/lib/actions/order.actions';
+import { notFound } from 'next/navigation';
+import { ShippingAddress } from '@/types';
+import OrderDetailsTable from './order-details-table';
 
 export const metadata = {
-    title: 'Order Details'
-}
+  title: 'Order Details',
+};
 
-const OrderDetailsPage = async (props: {
-    params: Promise<{id: string}>
-}) => {
+const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
+  const { id } = await props.params;
+  const order = await getOrderById(id);
+  if (!order) notFound();
 
-    const {id} = await props.params
-    const order = await getOrderById(id)
-    if (!order) notFound()
+  return (
+    <>
+      <OrderDetailsTable
+        order={{
+          ...order,
+          shippingAddress: order.shippingAddress as ShippingAddress,
+        }}
+      />
+    </>
+  );
+};
 
-    return ( <>
-    
-    
-{id}
-
-    </> );
-}
- 
 export default OrderDetailsPage;
