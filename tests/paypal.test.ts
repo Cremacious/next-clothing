19e403216134db856ef82cd938cc2376c99c076f
Cremacious <chrismackall3@gmail.com>
2025-02-25 18:29:34 -0500

    New profile routing started with new layout and main nav

--- tests/paypal.test.ts
+++ tests/paypal.test.ts
@@ -1,6 +1,6 @@
 import { generateAccessToken, paypal } from '../lib/paypal';
 
-// Test to generate access token form paypal
+// Test to generate access token from paypal
 test('generates token from paypal', async () => {
   const tokenResponse = await generateAccessToken();
   console.log(tokenResponse);
@@ -8,16 +8,20 @@ test('generates token from paypal', async () => {
   expect(tokenResponse.length).toBeGreaterThan(0);
 });
 
-test('creates paypal order', async () => {
+// Test to create a paypal order
+test('creates a paypal order', async () => {
   const token = await generateAccessToken();
   const price = 10.0;
+
   const orderResponse = await paypal.createOrder(price);
-  console.log(orderResponse);
-  expect(orderResponse).toHaveProperty('id')
-  expect(orderResponse).toHaveProperty('status')
-  expect(orderResponse.status).toBe('CREATED')
+  console.log('test', orderResponse);
+
+  expect(orderResponse).toHaveProperty('id');
+  expect(orderResponse).toHaveProperty('status');
+  expect(orderResponse.status).toBe('CREATED');
 });
 
+// Test to capture payment with mock order
 test('simulate capturing a payment from an order', async () => {
   const orderId = '100';
 
