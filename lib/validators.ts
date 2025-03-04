afd8ea628a6767164764eaec3a9cfcdddeb444b1
Cremacious <chrismackall3@gmail.com>
2025-03-04 18:05:22 -0500

    Integrated UploadThing api to enable images to be uploaded on product form

--- lib/validators.ts
+++ lib/validators.ts
@@ -18,7 +18,7 @@ export const insertProductSchema = z.object({
   brand: z.string().min(3, 'Brand must be three characters long.'),
   description: z.string().min(3, 'Description must be three characters long.'),
   stock: z.coerce.number(),
-  // images: z.array(z.string().min(1, 'At least one image is required')),
+  images: z.array(z.string().min(1, 'At least one image is required')),
   // isFeatured: z.boolean(),
   // banner: z.string().nullable(),
   price: currency,
